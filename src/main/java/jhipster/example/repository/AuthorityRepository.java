3ece0bf9ee9650151d739aa3b354085f245f2169
asaldana-fs <asaldana@financialsolutions.mx>
2021-06-10 15:06:25 -0500

    Initial version of Gateway generated by JHipster-7.0.1

---
+++ src/main/java/jhipster/example/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package jhipster.example.repository;
+
+import jhipster.example.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
